6cee35726945e7bd75e37d1b81e4ba44db2537c0
DiegoMarty <contacto@diegomarty.com>
2023-05-28 15:07:14 +0200

    💄 Updated root layout with more examples

--- app.js
+++ app.js
@@ -31,37 +31,13 @@ app.use((req, res, next) => {
 app.use(cors(corsOptions));
 */
 
-app.get("/", (req, res) => {
-    res.send(`
-      <div style="text-align: center; padding: 20px; font-family: Arial, sans-serif;">
-        <h1 style="color: #444;">Welcome to our Sprites API</h1>
-        <p style="color: #666;">You can make requests to the following routes:</p>
-        <ul style="list-style: none; padding-left: 0;">
-          <li style="margin-bottom: 10px;"><code style="background-color: #f9f9f9; padding: 5px; border-radius: 5px;">/sprites/*/?page=1&count=10</code> - Get the paths to a certain number of images (default 10) on a certain page in a specified folder. For example, /sprites/pokemon/versions/generation-v/black-white/animated/1/30 will return the paths to the first 30 images on page 1 in the folder sprites/pokemon/versions/generation-v/black-white/animated.</li>
-        </ul>
-
-        <p style="color: #666;">For all routes, :folder* represents the path to the folder containing the images, :page represents the page number (starting from 1), and :count represents the number of images to return (default 10 if not provided).</p>
-        <p style="color: #666;">You can make requests to get various Pokemon sprites.</p>
-        <p style="color: #666;">Here are some example endpoints you can use:</p>
-
-        <ul style="list-style: none; padding-left: 0;">
-          <li style="margin-bottom: 10px;"><code style="background-color: #f9f9f9; padding: 5px; border-radius: 5px;">/sprites/pokemon/versions/generation-v/black-white</code> - Get sprites from Pokemon Black and White</li>
-          <li style="margin-bottom: 10px;"><code style="background-color: #f9f9f9; padding: 5px; border-radius: 5px;">/sprites/pokemon/versions/generation-viii/icons</code> - Get icons from Pokemon Sword and Shield</li>
-          <!-- Continue with the rest of the generations and versions as needed -->
-        </ul>
-
-        <p style="color: #666;">Note: Replace any spaces in the path with %20 to ensure the URL is encoded correctly.</p>
-      </div>
-    `);
-});
-
 app.use("/sprites", express.static(path.join(__dirname, "/sprites")));
 
 app.get("/sprites/*", (req, res) => {
   console.log(req.params);
   if (!req.query.page) {
     return res.status(400).json({
-      error: "Missing required parameter: page"
+      error: "Missing required parameter: page",
     });
   }
 
@@ -125,7 +101,108 @@ app.get("/sprites/*", (req, res) => {
 });
 
 app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+  console.log("Server is running on port 5000");
+});
+
+app.get("/", (req, res) => {
+  res.send(`
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+    <div class="container mt-4">
+      <div class="jumbotron text-center">
+        <h1 class="display-4">Welcome to our Sprites API</h1>
+        <p class="lead">You can make requests to the following routes:</p>
+      </div>
+
+      <div class="mb-4">
+        <pre class="bg-light p-3 rounded">
+          <code>/sprites/*/?page=1&count=10</code>
+        </pre>
+        <p>This gets the paths to a certain number of images (default 10) on a certain page in a specified folder.</p>
+      </div>
+
+      <div id="accordion">
+        <div class="card">
+          <div class="card-header" id="headingOne">
+            <h5 class="mb-0">
+              <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                Obtención de Imágenes por Página y Cantidad - <code>/sprites/*/?page=1&count=10</code>
+              </button>
+            </h5>
+          </div>
+
+          <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-8">
+                  <pre class="bg-light p-3 rounded">
+                    <code>/sprites/pokemon/versions/generation-v/black-white</code>
+                  </pre>
+                  <p>This gets sprites from Pokemon Black and White.</p>
+                  <a href="${process.env.HOST}/sprites/pokemon/versions/generation-v/black-white/1.png">Click here</a> to try it.
+                </div>
+                <div class="col-4">
+                  <img src="${process.env.HOST}/sprites/pokemon/versions/generation-v/black-white/1.png" class="img-thumbnail" alt="Example image">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-header" id="headingTwo">
+            <h5 class="mb-0">
+              <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              Obtención de Sprites de Pokémon de la Generación V (Negro y Blanco) - Animated disponibles
+              </button>
+            </h5>
+          </div>
+
+          <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+            <div class="card-body">
+              <pre class="bg-light p-3 rounded">
+                <code>/sprites/pokemon/versions/generation-viii/icons</code>
+              </pre>
+              <p>This gets icons from Pokemon Sword and Shield.</p>
+              <!-- Note: Add an example link and image if available -->
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-header" id="headingThree">
+            <h5 class="mb-0">
+              <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                Obtención de Iconos de Pokémon de la Generación VIII (Espada y Escudo)
+              </button>
+            </h5>
+          </div>
+
+          <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+            <div class="card-body">
+            <p>This returns the paths to the first 10 images on page 1 in the specified folder. The paths are relative to "api-example".</p>
+            <pre class="bg-light p-3 rounded">
+                <code>
+                [
+                  {
+                    "ruta": "${process.env.HOST}/sprites/pokemon/versions/generation-v/black-white/0.png"
+                  },
+                  {
+                    "ruta": "${process.env.HOST}/sprites/pokemon/versions/generation-v/black-white/1.png"
+                  }
+                ]
+                </code>
+              </pre>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <p class="mb-4">Note: Replace any spaces in the path with %20 to ensure the URL is encoded correctly.</p>
+    </div>
+  `);
 });
 
 module.exports = app;
